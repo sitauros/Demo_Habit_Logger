e32fadd3bf3a201d0dd4e72b8eff7c4d11a697de
sitauros <50754341+sitauros@users.noreply.github.com>
2022-10-19 19:18:55 -0400

    Added sample companies when no records present in database

--- Habit Logger/GameEngine.cs
+++ Habit Logger/GameEngine.cs
@@ -6,6 +6,15 @@
         {
             Database.ConnectionString = "Data Source = UnicornPride.db";
             Database.CreateDatabase();
+
+            if (Database.GetRecordCount() == 0)
+            {
+                Database.AddNewCompany("CoarseLotions", "PHP", 5, "Mandatory afterwork self-training");
+                Database.AddNewCompany("ScandalWeb", "React", 1, "$400-700 USD/month during probation");
+                Database.AddNewCompany("FDM Troup", "DevOps", 2, "Will not verify employment at firms they subcontract you to");
+                Database.AddNewCompany("Revatorture", "Java", 3, "Must relocate to anywhere in home country");
+            }
+
             Menu.PrintMainMenu();
         }
     }
